6098429bc8690b446bc5d220f68c6f466237a60c
obedkin <aleksey.obedkin@rt.ru>
2019-12-13 19:29:13 +0300

    Test commit

---
+++ web/test.php
@@ -0,0 +1,17 @@
+<table>
+    <tr>
+        <td><span size=4><b>Дуэльное оружие</b></span></td>
+        <td><img src="images\Five-seveN_USG.jpg" width="150" height="150"></td>
+    </tr>
+    <tr><td><span size=4>Самозарядные пистолеты</span></td>
+        <td><img src="images/p.jpg" width="150" height="150"></td>
+    </tr>
+    <tr>
+        <td><span size=4>Автоматические пистолеты</span></td>
+        <td><img src="images/avtomat.jpg" width="150" height="150"></td>
+    </tr>
+    <tr>
+        <td><span size=4>Спортивно-целевые пистолеты</span></td>
+        <td><img src="images/p.jpg" width="150" height="150"></td>
+    </tr>
+</table>
