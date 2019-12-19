c35b3fb8a1000bf5326836599dea47522b766663
obedkin <aleksey.obedkin@rt.ru>
2019-12-19 14:07:18 +0300

    General: change DB passowrd on localhost

--- config/db.php
+++ config/db.php
@@ -1,11 +1,10 @@
 <?php
 
-
- return [
-     'class' => 'yii\db\Connection',
-     'dsn' => 'mysql:host=localhost;dbname=workshop',
-     'username' => 'workshop',
-     'password' => 'workshop',
-     'charset' => 'utf8',
- ];
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=workshop',
+    'username' => 'workshop',
+    'password' => 'k3BL1O%0k',
+    'charset' => 'utf8',
+];
 
