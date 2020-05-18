a4b964223e8f565d1adecaa4d681dcbd57d6e1e4
obedkin <obedkin@gmail.com>
2020-05-18 17:52:24 +0300

    JK: email path refactoring

--- modules/jk/mails/curator/email2curator.php
+++ modules/jk/mails/curator/email2curator.php
@@ -10,13 +10,13 @@ use yii\helpers\Url;
 <p>
     <strong>Уважаемый, <?= $curator->fio ?>!</strong><br>
     На портале <?=Html::a(Yii::$app->name,Url::home(true));?>
-    сотрудник <strong><?= $user->fio ?></strong> написал сообщение куратору жилищной кампании<br/>
-    Вам необходимо ответь на вопрос сотрудника
+    сотрудник <strong><?= $user->fio ?></strong> написал сообщение куратору жилищной кампании.
+    Вам необходимо ответь на вопрос сотрудника.
 </p>
 <ul>
     <li><strong>Сотрудник:</strong> <?= Html::a($user->fio, Url::base(true).Url::to('/user/' . $user->id)) ?></li>
     <li><strong>Сообщение:</strong> <?= $message->message ?></li>
 </ul>
 <p>
-    <?= Html::a('Ответить на сообщение', Url::base(true).Url::to(['/jk/message/','user'=>$user->id])) ?>
+    <?= Html::a('Ответить на сообщение', Url::base(true).Url::to('/jk/message?user='.$user->id)) ?>
 </p>
--- modules/jk/mails/curator/email2user.php
+++ modules/jk/mails/curator/email2user.php
@@ -10,14 +10,14 @@ use yii\helpers\Url;
 <p>
     <strong>Уважаемый, <?= $user->fio ?>!</strong><br>
     На портале <?=Html::a(Yii::$app->name,Url::home(true));?>
-    куратор <strong><?= $curator->fio ?></strong> написал ответ на ваше сообщение<br/>
+    куратор <strong><?= $curator->fio ?></strong> написал ответ на ваше сообщение.
     Вы можете вернуться на портал и просмотреть всю историю переписки
 </p>
 <ul>
     <li><strong>Сообщение:</strong> <?= $message->message ?></li>
 </ul>
 <p>
-    <?= Html::a('Просмотреть ответ куратора', Url::base(true).Url::to(['jk/curator/'])) ?>
+    <?= Html::a('Просмотреть ответ куратора', Url::base(true).Url::to('/jk/curator/')) ?>
 </p>
 
 <p>
