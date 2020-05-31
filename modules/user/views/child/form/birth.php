4c67d28a38ecaec3687ff88e603bcb076281bfd0
obedkin <obedkin@gmail.com>
2020-05-31 20:10:46 +0300

    USER: spouse mask in field form

--- modules/user/views/child/form/birth.php
+++ modules/user/views/child/form/birth.php
@@ -1,10 +1,21 @@
 <?php
 
 use yii\jui\DatePicker;
+use yii\widgets\MaskedInput;
 
 ?>
-<?= $form->field($model, 'birth_series')->textInput(['maxlength' => true]) ?>
-<?= $form->field($model, 'birth_number')->textInput(['maxlength' => true]) ?>
+<?= $form->field($model, 'birth_series')->widget(MaskedInput::class, [
+    'mask' => 'A-AA',
+    'clientOptions' => [
+        'clearIncomplete' => true,
+    ],
+]) ?>
+<?= $form->field($model, 'birth_number')->widget(MaskedInput::class, [
+    'mask' => '999999',
+    'clientOptions' => [
+        'clearIncomplete' => true,
+    ],
+]) ?>
 <?= $form->field($model, 'birth_date')->widget(
     DatePicker::class,
     [
@@ -18,8 +29,13 @@ use yii\jui\DatePicker;
         ],
     ]
 ) ?>
-<?= $form->field($model, 'birth_department')->textInput(['maxlength' => true]) ?>
-<?= $form->field($model, 'birth_code')->textInput(['maxlength' => true]) ?>
+<?= $form->field($model, 'birth_department')->textarea(['maxlength' => true]) ?>
+<?= $form->field($model, 'birth_code')->widget(MaskedInput::class, [
+    'mask' => '99999999',
+    'clientOptions' => [
+        'clearIncomplete' => true,
+    ],
+]) ?>
 <?= $form->field($model, 'birth_file_form', [
     'template' => getFileInputTemplate($model->birth_file, $model->attributeLabels()['birth_file'] . '.pdf'),
 ])->fileInput(['class' => 'custom-file-input']) ?>
--- modules/user/views/child/form/passport.php
+++ modules/user/views/child/form/passport.php
@@ -1,11 +1,23 @@
 <?php
 
 use yii\jui\DatePicker;
+use yii\widgets\MaskedInput;
 
 ?>
 
-<?= $form->field($model, 'passport_series')->textInput() ?>
-<?= $form->field($model, 'passport_number')->textInput() ?>
+<?= $form->field($model, 'passport_series')->widget(MaskedInput::class, [
+    'mask' => '9999',
+    'clientOptions' => [
+        'clearIncomplete' => true,
+    ],
+]) ?>
+
+<?= $form->field($model, 'passport_number')->widget(MaskedInput::class, [
+    'mask' => '999999',
+    'clientOptions' => [
+        'clearIncomplete' => true,
+    ],
+]) ?>
 <?= $form->field($model, 'passport_date')->widget(
     DatePicker::class,
     [
@@ -21,7 +33,12 @@ use yii\jui\DatePicker;
 ) ?>
 
 <?= $form->field($model, 'passport_department')->textarea() ?>
-<?= $form->field($model, 'passport_code')->textInput() ?>
+<?= $form->field($model, 'passport_code')->widget(MaskedInput::class, [
+    'mask' => '999-999',
+    'clientOptions' => [
+        'clearIncomplete' => true,
+    ],
+]) ?>
 <?= $form->field($model, 'passport_file_form', [
     'template' => getFileInputTemplate($model->passport_file, $model->attributeLabels()['passport_file'] . '.pdf'),
 ])->fileInput(['class' => 'custom-file-input'])?>
