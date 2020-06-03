27b52086b22d7d45f2fd1fa790f2cdd7a83c0171
obedkin <obedkin@gmail.com>
2020-06-04 00:16:26 +0300

    JK: order active tab error

--- modules/jk/views/order/form/form.php
+++ modules/jk/views/order/form/form.php
@@ -30,7 +30,7 @@ if (isset($model->is_mortgage)) {
                 <div class="card-header" data-intro="This is a tooltip!">
                     <h3 class="card-title"><i class="fas fa-ruble-sign"></i> Оформление заявки на участие в Жилищной Кампании</h3>
                 </div>
-                <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data', 'id' => 'jk-order']]); ?>
+                <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data', 'id' => 'jk-order','class'=>'form-tabs']]); ?>
                 <?= $form->field($model, 'percent_id', ['options' => ['class' => 'd-none']])->hiddenInput(); ?>
                 <?= $form->field($model, 'zaim_id', ['options' => ['class' => 'd-none']])->hiddenInput(); ?>
                 <div class="card-body">
