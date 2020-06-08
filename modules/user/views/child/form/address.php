2ead016ef606be774ca640b9dae310b14af810c3
obedkin <obedkin@gmail.com>
2020-06-08 16:34:24 +0300

    USER: child file address path

--- modules/user/views/child/form/address.php
+++ modules/user/views/child/form/address.php
@@ -38,13 +38,13 @@ use yii\helpers\Url;
     'template' => getFileInputTemplate($model->address_mother_file,  'Заявление от матери.pdf'),
 ])->fileInput(['class' => 'custom-file-input'])->hint('Заявление составляются полностью от руки. '
     . Html::a(Icon::show('file-pdf', ['framework' => Icon::FAR])
-        . 'Образец заявления', Url::to('/files/child/0-examples/example_child_address_mother.pdf', true), ['target' => '_blank'])) ?>
+        . 'Образец заявления', Url::base().Url::to('/files/child/0-examples/example_child_address_mother.pdf', true), ['target' => '_blank'])) ?>
 
 <?= $form->field($model, 'address_father_file_form', [
     'template' => getFileInputTemplate($model->address_father_file, 'Заявление от отца.pdf'),
 ])->fileInput(['class' => 'custom-file-input'])->hint('Заявление составляются полностью от руки. 
     Если в свидетельстве у ребёнка не указан отец, то заявление от папы не нужно. '
-    . Html::a(Icon::show('file-pdf', ['framework' => Icon::FAR]) . 'Образец заявления', Url::to('/files/child/0-examples/example_child_address_father.pdf', true), ['target' => '_blank'])) ?>
+    . Html::a(Icon::show('file-pdf', ['framework' => Icon::FAR]) . 'Образец заявления', Url::base().Url::to('/files/child/0-examples/example_child_address_father.pdf', true), ['target' => '_blank'])) ?>
 
 <?= $form->field($model, 'ejd_file_form', [
     'template' => getFileInputTemplate($model->ejd_file, $model->attributeLabels()['ejd_file'] . '.pdf'),
