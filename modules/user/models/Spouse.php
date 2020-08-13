e314a5e52156fef81c7fce377cc99520d8e4ffc7
aushanov <aleksey.ushanov@rt.ru>
2020-08-13 18:06:39 +0300

    JK: update - IE bug fix (/jk/order/create checkboxes)

--- modules/user/models/Spouse.php
+++ modules/user/models/Spouse.php
@@ -104,7 +104,7 @@ class Spouse extends Model
             [['date'], 'date', 'format' => 'php:d.m.Y', 'timestampAttribute' => 'date'],
             [['passport_date'], 'date', 'format' => 'php:d.m.Y', 'timestampAttribute' => 'passport_date'],
 
-            [['fio'], 'match', 'pattern' => '/^[а-яА-Я ]+$/u', 'message' => 'Неверный формат ФИО'],
+            [['fio'], 'match', 'pattern' => '/^[а-яА-Я ]+$/', 'message' => 'Неверный формат ФИО'],
 
             // Обязательные при наличии супруге
             [
