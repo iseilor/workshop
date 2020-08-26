a80e21764617f1a3a935ea4e96015293ce54b529
obedkin <obedkin@gmail.com>
2020-08-26 13:44:25 +0300

    MAIN: info about project

--- modules/main/views/default/about.php
+++ modules/main/views/default/about.php
@@ -1,161 +1,44 @@
 <?php
 
 use app\modules\main\Module;
+use kartik\icons\Icon;
+use yii\helpers\Html;
 
-$this->title = '<i class="fas fa-info"></i>'.' '.Module::t('module', 'About project');
-$this->params['breadcrumbs'][] = $this->title;?>
+$this->title = Icon::show('info') . Module::t('module', 'About project');
+$this->params['breadcrumbs'][] = $this->title; ?>
 
 <div class="row">
     <div class="col-12">
         <div class="invoice p-3 mb-3">
-            <!-- title row -->
             <div class="row">
                 <div class="col-12">
                     <h4>
-                        <i class="fas fa-globe"></i> Ростелеком, Москва.
-                        <small class="float-right">Дата: ыва</small>
+                        <i class="fas fa-globe"></i> ПАО Ростелеком, Макрорегиональный филиал "ЦЕНТР"
+                        <small class="float-right"><?= date('d.m.Y') ?></small>
                     </h4>
                 </div>
-                <!-- /.col -->
             </div>
-            <!-- info row -->
-            <div class="row invoice-info">
-                <div class="col-sm-4 invoice-col">
-                    From
-                    <address>
-                        <strong>Admin, Inc.</strong><br>
-                        795 Folsom Ave, Suite 600<br>
-                        San Francisco, CA 94107<br>
-                        Phone: (804) 123-5432<br>
-                        Email: info@almasaeedstudio.com
-                    </address>
-                </div>
-                <!-- /.col -->
-                <div class="col-sm-4 invoice-col">
-                    To
-                    <address>
-                        <strong>John Doe</strong><br>
-                        795 Folsom Ave, Suite 600<br>
-                        San Francisco, CA 94107<br>
-                        Phone: (555) 539-1037<br>
-                        Email: john.doe@example.com
-                    </address>
-                </div>
-                <!-- /.col -->
-                <div class="col-sm-4 invoice-col">
-                    <b>Invoice #007612</b><br>
-                    <br>
-                    <b>Order ID:</b> 4F3S8J<br>
-                    <b>Payment Due:</b> 2/22/2014<br>
-                    <b>Account:</b> 968-34567
-                </div>
-                <!-- /.col -->
-            </div>
-            <!-- /.row -->
-
-            <!-- Table row -->
             <div class="row">
-                <div class="col-12 table-responsive">
-                    <table class="table table-striped">
-                        <thead>
-                        <tr>
-                            <th>Qty</th>
-                            <th>Product</th>
-                            <th>Serial #</th>
-                            <th>Description</th>
-                            <th>Subtotal</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Call of Duty</td>
-                            <td>455-981-221</td>
-                            <td>El snort testosterone trophy driving gloves handsome</td>
-                            <td>$64.50</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>Need for Speed IV</td>
-                            <td>247-925-726</td>
-                            <td>Wes Anderson umami biodiesel</td>
-                            <td>$50.00</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>Monsters DVD</td>
-                            <td>735-845-642</td>
-                            <td>Terry Richardson helvetica tousled street art master</td>
-                            <td>$10.70</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>Grown Ups Blue Ray</td>
-                            <td>422-568-642</td>
-                            <td>Tousled lomo letterpress</td>
-                            <td>$25.99</td>
-                        </tr>
-                        </tbody>
-                    </table>
+                <div class="col-2">
+                    <?php
+                    echo  Html::img(
+                        Yii::$app->homeUrl .'logo/logo.png',
+                        [
+                            'class' => 'img-fluid pad',
+                        ]
+                    );
+                    ?>
                 </div>
-                <!-- /.col -->
-            </div>
-            <!-- /.row -->
-
-            <div class="row">
-                <!-- accepted payments column -->
-                <div class="col-6">
-                    <p class="lead">Payment Methods:</p>
-                    <img src="../../dist/img/credit/visa.png" alt="Visa">
-                    <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
-                    <img src="../../dist/img/credit/american-express.png" alt="American Express">
-                    <img src="../../dist/img/credit/paypal2.png" alt="Paypal">
-
-                    <p class="text-muted well well-sm shadow-none" style="margin-top: 10px;">
-                        Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem
-                        plugg
-                        dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
+                <div class="col-10">
+                    <p><a href="/">HR-Портал</a> макрорегионального филиала <strong>«Центр» ПАО «Ростелеком»</strong> позволяет в режиме реального времени:</p>
+                    <ul>
+                        <li>каждому работнику МРФ «Центр» видеть доступные для него льготы; рассчитывать их максимальный размер; вводить и отслеживать статус рассмотрения электронных заявок;</li>
+                        <li>ответственным по Программе работать с единым хранилищем документов; формировать в автоматическом режиме отчет «Предложения на жилищную комиссию»;</li>
+                        <li>руководителям проводить электронные согласования и принимать управленческие решения on-line.</li>
+                    </ul>
+                    <p>
+                        Программное обеспечение <a href="/">HR-Портала</a> разработано специалистами МРФ «Центр» на базе импортонезависимых технологий и решений.
                     </p>
-                </div>
-                <!-- /.col -->
-                <div class="col-6">
-                    <p class="lead">Amount Due 2/22/2014</p>
-
-                    <div class="table-responsive">
-                        <table class="table">
-                            <tbody><tr>
-                                <th style="width:50%">Subtotal:</th>
-                                <td>$250.30</td>
-                            </tr>
-                            <tr>
-                                <th>Tax (9.3%)</th>
-                                <td>$10.34</td>
-                            </tr>
-                            <tr>
-                                <th>Shipping:</th>
-                                <td>$5.80</td>
-                            </tr>
-                            <tr>
-                                <th>Total:</th>
-                                <td>$265.24</td>
-                            </tr>
-                            </tbody></table>
-                    </div>
-                </div>
-                <!-- /.col -->
-            </div>
-            <!-- /.row -->
-
-            <!-- this row will not appear when printing -->
-            <div class="row no-print">
-                <div class="col-12">
-                    <a href="invoice-print.html" target="_blank" class="btn btn-default"><i class="fas fa-print"></i> Print</a>
-                    <button type="button" class="btn btn-success float-right"><i class="far fa-credit-card"></i> Submit
-                        Payment
-                    </button>
-                    <button type="button" class="btn btn-primary float-right" style="margin-right: 5px;">
-                        <i class="fas fa-download"></i> Generate PDF
-                    </button>
                 </div>
             </div>
         </div>
