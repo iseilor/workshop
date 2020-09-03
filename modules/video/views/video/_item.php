2f847823b5e76aca1cdc079d1bceed91f94bdb6e
obedkin <obedkin@gmail.com>
2020-09-03 11:18:11 +0300

    VIDEO: update path to Sergey

--- modules/video/views/video/_item.php
+++ modules/video/views/video/_item.php
@@ -4,8 +4,8 @@
 
 <div class="card">
     <div class="card-body">
-        <a href="/files/video/<?= $model->video ?>" data-toggle="lightbox" data-title="<?= $model->title ?>" data-type="video">
-            <img class="img-fluid mb-1" src="/files/video/<?= $model->img ?>" style="border-radius: 0.25rem;">
+        <a href="<?=Yii::$app->homeUrl?>files/video/<?= $model->video ?>" data-toggle="lightbox" data-title="<?= $model->title ?>" data-type="video">
+            <img class="img-fluid mb-1" src="<?=Yii::$app->homeUrl?>files/video/<?= $model->img ?>" style="border-radius: 0.25rem;">
             <h6 style="text-align: center;"><?= $model->title ?></h6>
         </a>
     </div>
