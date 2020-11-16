47e69169e4d841884d06fbaa0b8703bd2dc257c7
obedkin <obedkin@gmail.com>
2020-11-16 21:40:31 +0300

    JK | HRCENTER-42: user repost manager email

--- modules/user/views/cabinet/order.php
+++ modules/user/views/cabinet/order.php
@@ -5,17 +5,20 @@ use app\components\grid\LinkColumn;
 
 use app\modules\jk\models\Order;
 use app\modules\jk\models\Status;
+use kartik\icons\Icon;
+use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\data\ActiveDataProvider;
 
 use yii\grid\GridView;
 
 $dataProvider = new ActiveDataProvider([
-    'query' => Order::find()->where(['created_by' => Yii::$app->user->identity->id,'deleted_at'=>null]),
+    'query' => Order::find()->where(['created_by' => Yii::$app->user->identity->id, 'deleted_at' => null]),
     'pagination' => [
         'pageSize' => 20,
     ],
 ]);
+\app\modules\jk\assets\JkOrderAsset::register($this);
 ?>
 <?= GridView::widget(
     [
@@ -36,7 +39,7 @@ $dataProvider = new ActiveDataProvider([
                 'value' => function ($data) {
                     $status = Status::findOne($data['status_id']);
                     return $status->getStatusLabel();
-                }
+                },
             ],
             [
                 'label' => 'Прогресс',
@@ -44,12 +47,27 @@ $dataProvider = new ActiveDataProvider([
                 'value' => function ($data) {
                     $status = Status::findOne($data['status_id']);
                     return $status->getProgressBar();
-                }
+                },
             ],
             [
                 'class' => ActionColumn::class,
                 'controller' => '/jk/order',
-                'template' => '{view} {update}',
+                'template' => '{manager} {view} {update}',
+                'buttons' => [
+
+                    // Кнопка повторной отправки письма руководителю, если заявка находится в статусе "Согласование руководителями"
+                    'manager' => function ($url, $model, $key) {
+                        if ($model->status->code == 'MANAGER_WAIT') {
+                            return Html::a(Icon::show('envelope'), $url, [
+                                'class' => 'btn btn-sm btn-warning btn-manager',
+                                'title' => 'Отправить повторное уведомление руководителю',
+                                'data-pjax' => '0',
+                            ]);
+                        } else {
+                            return '';
+                        }
+                    },
+                ],
             ],
         ],
     ]
