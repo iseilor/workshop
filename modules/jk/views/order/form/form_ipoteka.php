b779d0e51cd408c3793c18a0b22591e48d749988
aushanov <aleksey.ushanov@rt.ru>
2020-12-09 12:13:42 +0300

    JK: order form - ipoteka_grafic restyle

--- modules/jk/views/order/form/form_ipoteka.php
+++ modules/jk/views/order/form/form_ipoteka.php
@@ -134,11 +134,52 @@ if ($field_percent == '') {
                 ])->fileInput(['class' => 'custom-file-input', 'required' => ($zaim_required && !$model->ipoteka_file_bank_approval && $model->ipoteka_size != 0)]) ?>
             </div>
         </div>
-        <div class="col-md-4">
-            <?= $form->field($model, 'ipoteka_grafic', ['options' => ['class' => 'form-group ' . ($zaim_required ? 'd-none' : 'required'),
-                'required' => ($zaim_required ? false : true),]])
-                ->textarea(['rows' => '15']); ?>
-        </div>
+                <div class="col-md-4">
+                    <?= $form->field($model, 'ipoteka_grafic')->hiddenInput()->label(false)->hint(false); ?>
+                    <div class="card card-solid card-secondary">
+                        <div id="grafic_proc" class="card-body">
+
+                            <label>Сумма процентов по Ипотеке</label>
+
+                            <div class="row">
+                                <div class="col my-auto text-left">
+                                    <h6>Дата платежа: <span style="color:red">*</span></h6>
+                                </div>
+                                <div class="col my-auto text-center">
+                                    <h6>Сумма: (руб) <span style="color:red">*</span></h6>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col my-auto text-left percent-date">
+                                    <input type="text" class="form-control" name="ipoteka_date0" required/>
+                                </div>
+                                <div class="col my-auto text-center">
+                                    -
+                                </div>
+                                <div class="col my-auto text-center percent-summa">
+                                    <input type="text" class="form-control" name="ipoteka_summa0" readonly/>
+                                </div>
+                            </div>
+                            <div class="hint-block">Пожалуйста, введите дату первого платежа</div>
+                            <input type="hidden" id="prev_month" />
+                            <?php
+                            for ($i=1;$i<=10;$i++) {
+                                echo "<div class='row triggered' style='padding-top:5px; border-top:solid 0.375px #ced4da' hidden>
+                                        <div class='col my-auto text-left percent-date'>
+                                            <input type='text' class='form-control' name='ipoteka_date{$i}' readonly />
+                                        </div>
+                                        <div class='col my-auto text-center'>
+                                            -
+                                        </div>
+                                        <div class='col my-auto text-center percent-summa'>
+                                            <input type='text' class='form-control' name='ipoteka_summa{$i}' />
+                                        </div>
+                                       </div>";
+                            } ?>
+                        </div>
+                    </div>
+                </div>
 
     </div>
 
@@ -170,6 +211,105 @@ if ($field_percent == '') {
 <?php
 $script = <<< JS
 $(document).ready(function() {
+    {
+        var currentYear = new Date().getFullYear(),
+            minDate = new Date(currentYear, 1 - 1, 1),
+            maxDate = new Date(currentYear, minDate.getMonth() + 2, minDate.getDate() - 1);
+        $('#grafic_proc input[name="ipoteka_date0"]').datepicker({
+          minDate: minDate,
+          maxDate: maxDate
+        });
+        
+        $('#grafic_proc input[name="ipoteka_date0"]').on('change', function() {
+            var date = $('#grafic_proc input[name="ipoteka_date0"]').val(),
+                m = 0,
+                arr = date.split('.'),
+                prev_month = arr[1];
+            
+            if (date && ($('#grafic_proc .triggered:eq(0)').attr('hidden') == 'hidden')) {
+                $('#grafic_proc .hint-block').attr('hidden', true);
+                $('#grafic_proc input[name="ipoteka_summa0"]').attr({readonly: false, required: true});
+                
+                (arr[1] == "01") ? m = 9 : m = 8;
+                
+                
+                $("#grafic_proc .triggered").each(function(i, el){
+                    arr[1] = Number(arr[1]) + 1;
+                    if (arr[1] < 10) {
+                        arr[1] = "0" + arr[1];
+                    }
+                    
+                    $(this).attr('hidden', false);
+                    $(this).find('.percent-date input').val(arr.join('.'));
+                    $(this).find('.percent-summa input').attr('required', true);
+                    
+                    if (i == m) return false;
+                });
+                
+                $('#grafic_proc .percent-summa input').inputmask("decimal", {
+                    allowMinus: false, 
+                    allowPlus: false,
+                    digits: 2,
+                    rightAlign: false,
+                    groupSeparator: " ",
+                    radixPoint: ",",
+                    autoGroup: true,
+                });
+                
+            } else if (date && ($('#grafic_proc .triggered:eq(0)').attr('hidden') != 'hidden')) {
+                if (arr[1] == "02" && $('#prev_month').val() == "01") {
+                    $('#grafic_proc .triggered:last').attr('hidden', true);
+                    $('#grafic_proc .triggered:last .percent-summa input').attr('required', false);
+                    $('#grafic_proc .triggered:last .percent-date input').val('');
+                    m = 8;
+                } else if (arr[1] == "01" && $('#prev_month').val() == "02") {
+                    $('#grafic_proc .triggered:last').attr('hidden', false);
+                    $('#grafic_proc .triggered:last .percent-summa input').attr('required', true);
+                    m = 9;
+                } else {
+                    (arr[1] == "01") ? m = 9 : m = 8;
+                }
+                
+                $("#grafic_proc .triggered").each(function(i, el){
+                    arr[1] = Number(arr[1]) + 1;
+                    if (arr[1] < 10) {
+                        arr[1] = "0" + arr[1];
+                    }
+                    
+                    $(this).find('.percent-date input').val(arr.join('.'));
+                    
+                    if (i == m) return false;
+                });
+                
+            }
+            $('#prev_month').val(prev_month);
+        });
+        
+        $('#grafic_proc .row .percent-summa input').on('change', function() {
+            var all_filled = true
+                result = '';
+            
+            set = ($('#prev_month').val() == "02") ? $('#grafic_proc .row .percent-summa input:not(:last)') : $('#grafic_proc .row .percent-summa input');
+            
+            set.each(function(i, el){
+                    if (!$(this).val()) {
+                        all_filled = false;
+                        return false;
+                    }
+                });
+            if (all_filled) {
+                set = ($('#prev_month').val() == "02") ? $("#grafic_proc .row:not(:eq(0)):not(:last)"): $("#grafic_proc .row:not(:eq(0))");
+                
+                set.each(function(i, el){
+                    result_date = $(this).find('.percent-date input').val();
+                    result_summa = $(this).find('.percent-summa input').val();
+                    result += `\${result_date} - \${result_summa} руб\n`;
+                    $('#order-ipoteka_grafic').val(result);
+                });
+            }
+        });
+    }
+    
     var arr = [
                   //'.field-order-ipoteka_file_dogovor_form',
                   '.field-order-ipoteka_file_grafic_now_form',
