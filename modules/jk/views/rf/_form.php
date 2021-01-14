21b260822364f2241c8ad444004f27e03a80b7dd
obedkin <obedkin@gmail.com>
2021-01-14 19:08:40 +0300

    JK [bug-123]: js in RF form, not copy/paste, only autocomplite

--- modules/jk/views/rf/_form.php
+++ modules/jk/views/rf/_form.php
@@ -4,6 +4,7 @@ use kartik\icons\Icon;
 use yii\helpers\Html;
 use yii\jui\AutoComplete;
 use yii\web\JsExpression;
+use yii\web\View;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
@@ -11,32 +12,39 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="row">
-    <div class="col-md-12">
-        <div class="card card-primary">
-            <div class="card-header">
-                <h3 class="card-title"><?= Icon::show('sitemap') ?>Параметры РФа или МРФа</h3>
-                <?= Yii::$app->params['card']['header']['tools'] ?>
-            </div>
-            <?php $form = ActiveForm::begin(); ?>
-            <div class="card-body">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title"><?= Icon::show('sitemap') ?>Параметры РФа или МРФа</h3>
+                    <?= Yii::$app->params['card']['header']['tools'] ?>
+                </div>
+                <?php $form = ActiveForm::begin(); ?>
+                <div class="card-body">
 
-                <div class="row">
-                    <div class="col-md-4">
+                    <div class="row">
+                        <div class="col-md-4">
 
-                        <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
-                        <?= $form->field($model, 'description')->textarea(['rows' => 10]) ?>
-                    </div>
-                    <div class="col-md-4">
+                            <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+                            <?= $form->field($model, 'description')->textarea(['rows' => 10]) ?>
+                        </div>
+                        <div class="col-md-4">
 
 
-                        <?php
-                        $data = \app\modules\user\models\User::find()
-                            ->select(["concat(fio,' [', email,']') as value", "concat(fio,' [', email,']') as label", 'id as id','email as email','work_address as address','work_phone as phone'])
-                            ->asArray()
-                            ->all();?>
+                            <?php
+                            $data = \app\modules\user\models\User::find()
+                                ->select([
+                                    "concat(fio,' [', email,']') as value",
+                                    "concat(fio,' [', email,']') as label",
+                                    'id as id',
+                                    'email as email',
+                                    'work_address as address',
+                                    'work_phone as phone',
+                                ])
+                                ->asArray()
+                                ->all(); ?>
 
-                        <?= $form->field($model, 'user')->widget(
+                            <?= $form->field($model, 'user')->widget(
                                 AutoComplete::class, [
                                 'clientOptions' => [
                                     'source' => $data,
@@ -47,29 +55,53 @@ use yii\widgets\ActiveForm;
                                         $('#rf-email').val(ui.item.email);
                                         $('#rf-phone').val(ui.item.phone);
                                         $('#rf-address').val(ui.item.address);
-                                    }")
+                                    }"),
                                 ],
-                                'options'=>[
-                                    'class'=>'form-control'
-                                ]
-                            ]);
-                         ?>
-                        <?= $form->field($model, 'user_id',['options' => ['class' => 'd-none']])->hiddenInput() ?>
-                        <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
-                        <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
-                        <?= $form->field($model, 'address')->textarea(['rows' => 3]) ?>
-                    </div>
-                    <div class="col-md-4">
-                        <?= $form->field($model, 'coefficient')->textInput() ?>
-                        <?= $form->field($model, 'percent_max')->textInput() ?>
-                        <?= $form->field($model, 'loan_max')->textInput() ?>
+                                'options' => [
+                                    'class' => 'form-control',
+                                ],
+                            ])->hint('Автодополняемое поле. Начните писать ФИО, и система автоматически загрузит все остальные данные по сотруднику');
+                            ?>
+                            <?= $form->field($model, 'user_id', ['options' => ['class' => 'd-none']])->hiddenInput() ?>
+                            <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+                            <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+                            <?= $form->field($model, 'address')->textarea(['rows' => 3]) ?>
+                        </div>
+                        <div class="col-md-4">
+                            <?= $form->field($model, 'coefficient')->textInput() ?>
+                            <?= $form->field($model, 'percent_max')->textInput() ?>
+                            <?= $form->field($model, 'loan_max')->textInput() ?>
+                        </div>
                     </div>
                 </div>
+                <div class="card-footer">
+                    <?= Html::submitButton(Icon::show('save') . Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+                </div>
+                <?php ActiveForm::end(); ?>
             </div>
-            <div class="card-footer">
-                <?= Html::submitButton(Icon::show('save') . Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
-            </div>
-            <?php ActiveForm::end(); ?>
         </div>
     </div>
-</div>
+
+<?php
+
+/**
+ * Запрещаем сотруднику использоваться copy/paste
+ * Стираем все поле, если он нажал DELETE/Backspace
+ * Стираем скрытое поле, если он пытается всё набрать на клавиатуре
+ */
+$this->registerJs(
+    "$(document).ready(function () {
+        $('#rf-user').bind('copy paste', function (e) {
+            e.preventDefault();
+        });
+        $('#rf-user').keypress(function() {
+              $('#rf-user_id').val('');
+              var keycode = (event.keyCode ? event.keyCode : event.which);
+              if(keycode == '8' || keycode == '46'){
+                $('#rf-user').val('');
+              }
+        });
+    });",
+    View::POS_READY,
+    'rf-form'
+);
