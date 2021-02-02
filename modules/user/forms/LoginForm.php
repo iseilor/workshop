63a326fc012536b80d26836cae6f7638093a2dd1
obedkin <obedkin@gmail.com>
2021-02-02 10:29:32 +0300

    JK: AD not phone

--- modules/user/forms/LoginForm.php
+++ modules/user/forms/LoginForm.php
@@ -127,7 +127,7 @@ class LoginForm extends Model
         $user->position = $this->userAD->title[0];
         $user->work_department = $this->userAD->department[0];
         $user->work_department_full = $this->userAD->extensionattribute2[0];
-        $user->work_phone = $this->userAD->telephonenumber[0];
+        $user->work_phone = (isset($this->userAD->telephonenumber[0]))?$this->userAD->telephonenumber[0]:'';
         $user->work_address = $this->userAD->extensionattribute11[0];
         $user->department_id = 1;
 
