337d250c43e68a771afb06781e7ea9e98d3ab082
solenev <s.olenev@globus-telecom.ru>
2021-05-26 15:52:16 +0300

    test com

--- views/layouts/index.php
+++ views/layouts/index.php
@@ -41,6 +41,7 @@ AppAssetAdminLTE::register($this);
         <?= $this->render('assets/sidebar'); ?>
 
         <div class="content-wrapper">
+
             <div class="content-header">
                 <div class="container-fluid">
                     <div class="row mb-2">
@@ -72,7 +73,6 @@ AppAssetAdminLTE::register($this);
                     <?= Alert::widget() ?>
 
                     <!-- Согласования про жилищной программе -->
-					
                     <?php if (!Yii::$app->user->isGuest && Agreement::orderCount()>0): ?>
                         <div class="alert alert-primary alert-dismissible">
                             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
